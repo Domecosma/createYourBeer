fa535791e4bb3005bf0769cdae9284dcbe95fd8e
Domenico <domij96@hotmail.it>
2023-12-03 16:31:49 +0100

    Created beer class and properties

---
+++ src/createyourbeer/Beer.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package createyourbeer;
+
+/**
+ *
+ * @author michi
+ */
+public class Beer {
+    
+    String name;
+    String taste;
+    int quantity;
+    String colour;
+}
